a46172c98433ca7dce26dae7402a4f9863282ac8
Nguhi <nguhi.dev@gmail.com>
2025-10-15 13:29:12 +0300

    :bug: fix munit unit tests

--- Enums/main.c
+++ Enums/main.c
@@ -5,38 +5,37 @@
 
 #include "color.h"
 
-static MunitResult test_color_enum1(const MunitParameter params[], void* data) {
-  (void) params;
-  (void) data;
+MunitResult test_color_enum1(const MunitParameter params[], void *data) {
+    (void) params;
+    (void) data;
 
-  munit_assert_int(RED, ==, 0);
-  munit_assert_int(GREEN, ==, 1);
-  munit_assert_int(BLUE, ==, 2);
+    munit_assert_int(RED, ==, 0);
+    munit_assert_int(GREEN, ==, 1);
+    munit_assert_int(BLUE, ==, 2);
 
-  return MUNIT_OK;
+    return MUNIT_OK;
 }
 
-static MunitResult test_color_enum2(const MunitParameter params[], void* data) {
-  (void) params;
-  (void) data;
+MunitResult test_color_enum2(const MunitParameter params[], void *data) {
+    (void) params;
+    (void) data;
 
-  munit_assert_int(RED, !=, 4);
-  munit_assert_int(GREEN, !=, 2);
-  munit_assert_int(BLUE, !=, 0);
+    munit_assert_int(RED, !=, 4);
+    munit_assert_int(GREEN, !=, 2);
+    munit_assert_int(BLUE, !=, 0);
 
-  return MUNIT_OK;
+    return MUNIT_OK;
 }
 
-static  MunitTest tests[] = {
-  { "/are_defined", test_color_enum1, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
-  { "/are_defined_correctly", test_color_enum2, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
-  { NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL } // sentinel
+static MunitTest tests[] = {
+    {"/are_defined", test_color_enum1, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
+    {"/are_defined_correctly", test_color_enum2, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
+    /* Mark the end of the array with an entry where the test
+        * function is NULL */
+    {NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL} // sentinel
 };
 
-int main(int argc, char* argv[]) {
-
-
-  const MunitSuite suite = {
+static const MunitSuite suite = {
     "colors",
     tests,
     NULL,
@@ -44,6 +43,7 @@ int main(int argc, char* argv[]) {
     MUNIT_SUITE_OPTION_NONE
 };
 
-  return munit_suite_main(&suite, NULL, argc, argv);
-  // return munit_suite_main(&suite, (void*) "µnit", argc, argv);
+
+int main(int argc, char *argv[]) {
+    return munit_suite_main(&suite, NULL, argc, argv);
 }
